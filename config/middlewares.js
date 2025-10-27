976b51835a989a9901d20f93cd01a079b2fcb88d
franlopez499 <56626606+franlopez499@users.noreply.github.com>
2025-10-27 17:07:39 +0100

    Refactor logger and CORS middleware configurations in middlewares.js to include debug level logging and support multiple origins for enhanced flexibility.

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,12 +1,20 @@
 module.exports = [
-  'strapi::logger',
+  {
+    name: 'strapi::logger',
+    config: {
+      level: 'debug', // default is info
+    },
+  },
   'strapi::errors',
   'strapi::security',
   {
     name: 'strapi::cors',
     config: {
       enabled: true,
-      origin: ['https://strapi.inpublic.es'],
+      origin: (ctx) => {
+        // Accept requests from your real frontend or Cloudflare
+        return ['https://strapi.inpublic.es', 'https://*.inpublic.es'];
+      },
       headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
       credentials: true,
     },
