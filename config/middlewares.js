0523338efca0ed8ab3111773d995594c8ac2eca8
franlopez499 <56626606+franlopez499@users.noreply.github.com>
2025-10-29 19:31:05 +0100

    Enhance security configurations in admin and middleware settings. Updated cookie settings to include httpOnly for better security. Improved content security policy directives in middlewares for stricter resource loading controls.

--- config/middlewares.js
+++ config/middlewares.js
@@ -6,14 +6,35 @@ module.exports = [
     },
   },
   'strapi::errors',
-  'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'https://market-assets.strapi.io'],
+          'media-src': ["'self'", 'data:', 'blob:'],
+          'default-src': ["'self'"],
+          'base-uri': ["'self'"],
+          'font-src': ["'self'", 'https:', 'data:'],
+          'form-action': ["'self'"],
+          'frame-ancestors': ["'self'"],
+          'object-src': ["'none'"],
+          'script-src': ["'self'"],
+          'script-src-attr': ["'none'"],
+          'style-src': ["'self'", 'https:', "'unsafe-inline'"],
+        },
+      },
+    },
+  },
   {
     name: 'strapi::cors',
     config: {
       enabled: true,
       origin: ['https://strapi.inpublic.es'],
-      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
-      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept', 'X-Requested-With'],
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS', 'HEAD'],
       credentials: true,
     },
   },
